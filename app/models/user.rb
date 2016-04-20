4ec5b0bdeebd5e469d267439d618abf3c3cee6b1
AnichinDaniil <anichindaniil@gmail.com>
2016-04-20 20:36:58 +0300

    Additional User methods for vievs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ActiveRecord::Base
 	has_many :quests, through: :user_quests
 	has_secure_password
     validates_uniqueness_of :email
+
+
+    @@delta = 10
+    
+    def self.xp_from_level(level)
+      if level >= 2
+        xp = @@delta / 2 * (level + 2) * (level - 1)
+      else
+        xp = 0
+      end
+      return xp
+    end
+    
+    def self.level_from_xp(xp)
+      level = 0
+      index = 2
+      while xp >= 0
+        xp -= index * @@delta;
+        level += 1;
+        index += 1;
+      end
+      return level
+    end
+
+    def level
+        return self.class.level_from_xp(self.experience)
+    end
+
+    def progress
+    	previous_xp = self.class.xp_from_level(self.level)
+    	next_xp = self.class.xp_from_level(self.level + 1)
+        return 100 * (self.experience - previous_xp) / (next_xp - previous_xp)
+    end
 end
